5c7a1cdbeb67f3cdb3c6a762317af24b981dab48
jack.squirrel <t.ismagilov@longgammalabs.com>
2026-01-10 14:21:48 +0300

    Use v1 lob abi for old contracts

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -1,5 +1,6 @@
 export { erc20Abi } from './erc20';
 export { lobAbi } from './lob';
+export { lobV2Abi } from './lobV2';
 export { erc20PermitAbi } from './erc20permit';
 export { erc20WethAbi } from './erc20Weth';
 export { lpManagerAbi } from './lpManager';
